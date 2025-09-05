491486f51d632b1a441e733842a9d7a1d023b13c
lndsilva <lndsilva@hotmail.com>
2025-09-05 11:39:13 -0300

    fragment com bottomNav

--- app/src/main/java/br/com/etecia/myapp/ChatActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ChatActivity.java
@@ -1,14 +1,21 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ChatActivity extends AppCompatActivity {
+    BottomNavigationView idChatBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,43 @@ public class ChatActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        idChatBottomNav = findViewById(R.id.idChatBottomNav);
+
+        idChatBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            Fragment select_fragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.mChatConversas) {
+                    select_fragment = new ConversasFragment();
+                }
+                if (id == R.id.mChatAtualizacoes) {
+                    select_fragment = new AtualizacoesFragment();
+                }
+                if (id == R.id.mChatComunicaoes) {
+                    select_fragment = new ComunicacoesFragment();
+                }
+                if (id == R.id.mChatLigacoes) {
+                    select_fragment = new LigacoesFragment();
+                }
+
+                if (select_fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, select_fragment).commit();
+
+                }
+
+                return true;
+            }
+
+
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConversasFragment()).commit();
+
+        }
     }
 }
--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,14 +1,20 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
